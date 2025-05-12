c5b787840b18aed14ad61e5908bdb7daedae0668
sonnywhitehead <sonny.whitehead@gmail.com>
2025-05-12 14:41:29 -0500

    Added Spaecship and Asteroids

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-import constants
+from constants import *
+from player import Player
+from asteroidfields import AsteroidField
+from asteroid import Asteroid   
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
 
-        screen.fill((0, 0, 0))
-        fill(screen, (0, 0, 0))
-    
-    display.flip() 
-                
-    
-    
-    
-    print ("Starting Asteroids!")
-    print (f"Screen width: {constants.SCREEN_WIDTH}")
-    print (f"Screen height: {constants.SCREEN_HEIGHT}")
+        updatable.update(dt)
+
+        screen.fill("black")
+        for drawables in drawable:
+            drawables.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
